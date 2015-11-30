a77fa65ab1eb17b0794d540424e9adb80d3e7206
Ethan Davies <Ethan@Ethans-MacBook-Pro.local>
2015-11-30 23:25:35 +0000

    Added methods and variables to Board & Tile

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,5 +1,82 @@
 package game;
 
-public class Board {
+import java.awt.*;
+import java.util.*;
 
+/**
+ * 
+ * @author Ethan Davies
+ *
+ */
+
+public class Board {
+	
+	private int boardPosition;
+	private int rows;
+	private int columns;
+	private int mineCount;
+	private Boolean gameWon;
+	private Boolean gameLost;
+	private ArrayList<String> tiles = new ArrayList<String>();
+	
+	public static void main(String[] args) {
+	
+	}
+	
+	public Boolean isHidden() {
+		// Returns true or false is tile is hidden
+	}
+	
+	public Boolean isDefused() {
+		// Returns true or false is tile is defused
+	}
+	
+	public Boolean Mine() {
+		// Returns true or false if tile is a mine
+	}
+	
+	public Boolean isRevealed() {
+		// Returns true or false if the tile is revealed
+	}
+
+	public void placeMines() {
+		// This places the mines in random areas on the board(In the array list)
+	}
+	
+	public revealTile(int boardPosition) {
+		// This method is responsible for the revealing of a tile on the board
+	}
+	
+	private void paintBoard(Graphics g) {
+		// This will be responsible for creating the graphics of the board
+	}
+	
+	private void flagTile(int boardPosition) {
+		// This method will be responsible for flagging a tile on the board
+	}
+	
+	public Boolean isGameLost() {
+		// Checks the conditions to see if the game has been lost
+	}
+	
+	public Boolean isGameWon() {
+		// Checks the conditions to see if the game has been won
+	}
+	
+	private void setBoardDimensions(int rows, int columns) {
+		// Sets the dimensions HxW of the board that is to be created
+	}
+	
+	private void resetBoard() {
+		// This will reset the board to an empty board when executed
+	}
+	
+	public void startNewGame() {
+		// This will start a new game of Kablewie
+	}
+	
+	public void populateBoard() {
+		// This will populate the board based on the dimensions specified with mines and hidden tiles
+	} 
 }
+	
--- src/game/Tile.java
+++ src/game/Tile.java
@@ -1,5 +1,40 @@
 package game;
 
-public class Tile {
+import java.awt.*;
+import java.util.*;
 
+/**
+ * 
+ * @author Ethan Davies
+ *
+ */
+
+public class Tile {
+	
+	private Boolean isClicked;
+	private Boolean mineType;
+	private Boolean hiddenType;
+	private Boolean revealedType;
+	private Boolean defusedType;
+	private Image tileCover;
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public void setTileImage(Image tileCover) {
+		// This will set the image of each tile to be displayed on the board
+	}
+	
+	public void createTile() {
+		// This will create each individual tile required
+	}
+	
+	public void changeTileType() {
+		// This will change the type of the tile if required
+	}
+	
+	public void render() {
+		// This will render the tile 
+	}
 }
