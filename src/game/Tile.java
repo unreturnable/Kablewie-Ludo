63d4040b2547eb74e3e31d645ff1d4a2810e3d91
Tom Phelps <Tom@Thomass-MacBook-Air.local>
2015-12-02 18:13:21 +0000

    Completed methods for createTile and setTileImage

--- src/game/Tile.java
+++ src/game/Tile.java
@@ -2,6 +2,10 @@ package game;
 
 import java.awt.*;
 import java.util.*;
+import java.awt.event.ActionListener;
+
+import javax.swing.Icon;
+import javax.swing.JButton;
 
 /**
  * 
@@ -9,7 +13,7 @@ import java.util.*;
  *
  */
 
-public class Tile {
+public class Tile{
 	
 	private Boolean isClicked;
 	private Boolean mineType;
@@ -17,16 +21,22 @@ public class Tile {
 	private Boolean revealedType;
 	private Boolean defusedType;
 	private Image tileCover;
+	private JButton tile = new JButton();
 	
 	public static void main(String[] args) {
 		
 	}
 	
 	public void setTileImage(Image tileCover) {
+		tile.setIcon((Icon) tileCover);
 		// This will set the image of each tile to be displayed on the board
 	}
 	
 	public void createTile() {
+		tile = new JButton();
+        tile.setPreferredSize(new Dimension(20,20));
+        tile.setMargin(new Insets(0,0,0,0));
+        setTileImage(tileCover);
 			}
 	
 	public void changeTileType() {
