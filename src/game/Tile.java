6af46fd844741698fd025598094a78195b26453e
unreturnable <unreturnable@sucs.org>
2015-12-03 15:24:37 +0000

    Updated Tile constants

--- src/game/Tile.java
+++ src/game/Tile.java
@@ -9,8 +9,8 @@ import java.awt.*;
 
 public class Tile {
 	
-	protected static int width = 50;
-	protected static int height = 50;
+	protected static int WIDTH = 50;
+	protected static int HEIGHT = 50;
 
 	protected Boolean isClicked;
 	protected Boolean mineType;
