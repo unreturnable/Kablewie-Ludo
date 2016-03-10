396d5a173b8e240ca14be45df0f3bca39daeee5e
meffersino <michaelpjeffrey@hotmail.co.uk>
2016-03-10 17:19:17 +0000

    Sorted out magic numbers in kablewie

--- src/main/Kablewie.java
+++ src/main/Kablewie.java
@@ -22,6 +22,12 @@ import game.Player;
 public class Kablewie {
 
 	private JFrame m_frame;
+	private final int FRAME_WIDTH = 640;
+	private final int FRAME_HEIGHT = 480;
+	private final int MAX_X_COORD = 2;
+	private final int MAX_Y_COORD = 2;
+	private final int SCREEN_WIDTH_DIV = 2;
+	private final int SCREEN_HEIGHT_DIV = 2;
 
 	/**
 	 * Initialisation method.
@@ -45,9 +51,11 @@ public class Kablewie {
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 
 		// Set frame boundaries.
-		m_frame.setSize(640, 480);
-		m_frame.setLocation(dim.width / 2 - m_frame.getSize().width / 2, 
-				dim.height / 2 - m_frame.getSize().height / 2);
+		m_frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		m_frame.setLocation(
+				dim.width / MAX_X_COORD - m_frame.getSize().width / 
+				SCREEN_WIDTH_DIV, dim.height / MAX_Y_COORD - 
+				m_frame.getSize().height /SCREEN_HEIGHT_DIV);
 
 		// Set window to close when exited.
 		m_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
